2319d4cbf78b9affcf2076a0ba47bf0a54d63ab5
yunabc <450387582@qq.com>
2018-04-03 14:51:18 +0800

    添加依赖 添加验证

--- src/assets/js/validate.js
+++ src/assets/js/validate.js
@@ -9,6 +9,17 @@ class Validate {
       callback();
     }
 	}
+  emailRequired (rule, value, callback){
+    debugger;
+    if (typeof value == 'undefined' || value == '') {
+      callback(new Error('Enter you email'));
+    } else {
+      if(!/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(value)){
+        callback(new Error('Incorrect email format'));
+      }
+      callback();
+    }
+  }
 }	
 
 export default new Validate()
